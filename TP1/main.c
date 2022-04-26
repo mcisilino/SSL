01d241d01262b4b484d2a73d040c9e40a002ff66
mcisilino <83595094+mcisilino@users.noreply.github.com>
2022-04-26 19:18:19 -0300

    Add files via upload

---
+++ TP1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+int main()
+{
+    // TP1 - MICAELA CISILINO
+
+    int c=getchar();
+
+    while (c!=EOF) {
+        if (islower(c)) {
+            c=toupper(c);
+            putchar(c);
+        }
+        else if (isupper(c)) {
+            c=tolower(c);
+            putchar(c);
+        }
+        else if(isdigit(c)==NULL) {
+            putchar(c);
+        }
+        c=getchar();
+    }
+}
